55c1c08b44773fe59c8ecd8f4e8594b23cfe768b
David Donahue <david.donahue@mines.sdsmt.edu>
2018-11-01 22:45:57 -0600

    Created WaveFrontAI

---
+++ WaveFrontAI.h
@@ -0,0 +1,36 @@
+#ifndef __WAVE_FRONT_AI_H
+#define __WAVE_FRONT_AI_H
+
+#include "src/Actor.h"
+
+class WaveFrontAI : public Actor
+{
+    int calcDist(int x1, int y1, int x2, int y2);
+
+public:
+
+    /**
+     * calculates a move, and returns the data describing its move
+     * @return MoveData the desired move to be made, if it is possible then the gamefield will do the move
+     */
+    virtual direction move(MapData map, PositionData status);
+
+    /**
+     * calculates an attack and returns the data describing its attack
+     * @return Attack data the desired attack to be made, if possible the gamefield will do the move
+     */
+    virtual direction attack(MapData map, PositionData status);
+
+    virtual attributes setAttribute(int pointsAvailable);
+
+    virtual int spendAP(MapData map, PositionData status);
+
+    //generates a wavefront map for pathfinding
+    std::vector<int> waveFront (MapData map, int tx, int ty);
+
+    WaveFrontAI();
+    ~WaveFrontAI();
+};
+
+
+#endif
---
+++ tests/WaveFrontAITests.cpp
@@ -0,0 +1,13 @@
+#include "catch.hpp"
+#include "../WaveFrontAI.h"
+#include <vector>
+
+TEST_CASE("WaveFrontAI constructs")
+{
+    REQUIRE(sizeof(WaveFrontAI) > 0);
+}
+
+TEST_CASE("WaveFrontMap works on an open field")
+{
+    
+}
